7d1cd1110f067e227bac1432fbec933c20941788
anan <675988829@qq.com>
2017-10-14 00:52:41 +0800

    0.0.9

---
+++ docs/js/main.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    mousewheelControl: true,
+    speed: 300,
+});
+
+var swiperBusiness = new Swiper('.swiper-container-business', {
+    loop: false,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30
+});
+
+var swiperSucceed = new Swiper('.swiper-container-succeed', {
+    loop: false,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30
+});
+
+var swiperCooperation = new Swiper('.swiper-container-cooperation', {
+    loop: false,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30
+});
+
+var swiperUs = new Swiper('.swiper-container-us', {
+    loop: false,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30
+});
